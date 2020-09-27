a4ecebd88ec4ec2aeead184cd1e063184521429d
Jibin Koshy <jkoshy@houjkoshymbp.eogresources.com>
2020-09-27 07:08:22 -0500

    add react-router-dom and Checkout page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 const App: React.FC = () => {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/Checkout.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Checkout.css';
+
+const Checkout: React.FC = () => {
+  return <div> Inside Checkout page</div>;
+};
+
+export default Checkout;
